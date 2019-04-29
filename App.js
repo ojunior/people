98feb35fabe67a3c171b8937c4d1d376698d724d
unknown <odeidemoreira@gmail.com>
2019-04-29 11:56:56 -0300

    Criando projeto

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './src/components/Header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View >
+        <Header />
+      
+      </View>
+    );
+  }
+}
